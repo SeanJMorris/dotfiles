c79d8b3f7b461cdd1191f5551dc27acd1a2520df
Sean Morris <48961187+SeanJMorris@users.noreply.github.com>
2026-03-18 22:59:30 -0400

    Suppress output when adding homebrew-installed zsh to valid shells

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -4,7 +4,7 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 
 # Add homebrew-installed zsh to the list of valid shells
 echo "Enter superuser (sudo) password to edit /etc/shells"
-echo /home/linuxbrew/.linuxbrew/bin/zsh | sudo tee -a /etc/shells
+echo /home/linuxbrew/.linuxbrew/bin/zsh | sudo tee -a /etc/shells >/dev/null
 
 # Change shell to latest version that homebrew has set up
 echo "Enter user password to change login shell"
